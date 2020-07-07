510bd0c955b796ed5ec28ee1607b49baaa270a6f
Casper Wilkes <casper@casperwilkes.net>
2020-07-07 17:23:56 -0400

    Updated web auth routes

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+// Auth::routes();
 
-// Route::get('/home', 'HomeController@index')->name('home');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+Route::post('register', 'Auth\RegisterController@register');
+/**
+ * @todo bring in rest of auth routes
+ */
 
-Auth::routes();
-
-Route::view('/{path?}', 'layouts.app')
+// Master route //
+Route::view('/{path?}', 'home')
      ->where('path', '.*');
